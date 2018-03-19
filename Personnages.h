a0045651025bd53d1a1921fdc9bd301e4b80f124
Clément Pera <clemes753@gmail.com>
2018-03-19 21:42:53 +0100

    Add classes

--- Personnages.h
+++ Personnages.h
@@ -1,6 +1,24 @@
-#ifndef PERSONNAGES_H_INCLUDED
-#define PERSONNAGES_H_INCLUDED
+#ifndef DEF_PERSONNAGE
+#define DEF_PERSONNAGE
 
+#include <string>
 
+class Personnage
+{
+    public:
 
-#endif // PERSONNAGES_H_INCLUDED
+    void recevoirDegats(int nbDegats);
+    void attaquer(Personnage &cible);
+    void boirePotionDeVie(int quantitePotion);
+    void changerArme(std::string nomNouvelleArme, int degatsNouvelleArme);
+    bool estVivant();
+
+    private:
+
+    int m_vie;
+    int m_mana;
+    std::string m_nomArme; //Pas de using namespace std, il faut donc mettrestd:: devant string
+    int m_degatsArme;
+};
+
+#endif
