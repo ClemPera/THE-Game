d1b1ec73ed6cde82f7c0ea1385fe5c9f23167909
Clément Pera <clemes753@gmail.com>
2018-03-21 13:08:47 +0100

    Modify some things...

--- main.cpp
+++ main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
-#include "Personnage.h" //Ne pas oublier
+#include <string>
+#include "Personnage.h"
 
 using namespace std;
 
+
 int main()
 {
-    Personnage david, goliath;
-    //Création de 2 objets de type Personnage : david et goliath
+    // Création des personnages
+    Personnage david, goliath("Epée aiguisée", 20);
 
-    goliath.attaquer(david); //goliath attaque david
-    david.boirePotionDeVie(20); //david récupère 20 de vie en buvant une potion
-    goliath.attaquer(david); //goliath réattaque david
-    david.attaquer(goliath); //david contre-attaque... c'est assez clair non ?
+    // Au combat !
+    goliath.attaquer(david);
+    david.boirePotionDeVie(20);
+    goliath.attaquer(david);
+    david.attaquer(goliath);
     goliath.changerArme("Double hache tranchante vénéneuse de la mort", 40);
     goliath.attaquer(david);
+
+    // Temps mort ! Voyons voir la vie de chacun...
+    cout << "David" << endl;
+    david.afficherEtat();
+    cout << endl << "Goliath" << endl;
+    goliath.afficherEtat();
 
     return 0;
 }
